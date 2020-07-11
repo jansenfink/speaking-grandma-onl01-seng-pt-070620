5c38b5d4e09f052be58479641bae2aeefd8495ca
Jansen Fink <jansenfink@charter.net>
2020-07-11 13:00:43 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,7 +17,8 @@ def speak_to_grandma(phrase)
     return "HUH?! SPEAK UP, SONNY!"
   elsif phrase == phrase.upcase && phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN"
-  else "NO, NOT SINCE 1938!"
+  elsif phrase == phrase.upcase && != "I LOVE YOU GRANDMA!"
+  return "NO, NOT SINCE 1938!"
   end
 end  
     
